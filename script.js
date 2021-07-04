431ef7dbdb92a3b87ba0924ef10be2fbaf6f6c41
Mohammed <mohammedq91@gmail.com>
2021-07-04 18:17:34 -0400

    Activating keyboard arrow to change snake direction

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var canvas;
 var canvasContext;
-var ballX = 50;
-var ballY = 50;
+var snakeX = 50;
+var snakeY = 20;
+
+var appleX = 100;
 
 window.onload = function (){
   canvas = document.getElementById('gameCanvas');
@@ -11,21 +13,16 @@ window.onload = function (){
   setInterval(function(){
     drawEverything();
     moveSnake();
+    
   }, 1000/framesPerSecond);
-  
+
+  changeSnakeDirection();
 }
 
-function drawEverything(){
-  moveSnake()
-  
+function drawEverything(){  
   colorRect(0,0, canvas.width, canvas.height,'black');
-  colorRect(ballX, canvas.height/3, 30, 30,'red');
-  colorCircle(ballX,canvas.height/2, 30, 30, 'green');
-  
-  // colorCircle(canvas.width/3,canvas.height/2, 30, 30, 'red');
-  
-  // canvasContext.fillStyle = 'red';
-  // canvasContext.fillRect(canvas.width/3,canvas.height/2, 30, 30)
+  colorRect(snakeX, canvas.height/2, 20, 20,'green');
+  colorCircle(appleX,canvas.height/3, 10, 'red');
 }
 
 function colorRect(leftX, topY, width, height, drawColor){
@@ -33,63 +30,102 @@ function colorRect(leftX, topY, width, height, drawColor){
   canvasContext.fillRect (leftX, topY, width, height);
 }
 
-function colorCircle (centerX, centerY, radius, drawCircle){
-  canvasContext.fillStyle = drawCircle;
+function colorCircle (centerX, centerY, radius, drawColor){
+  canvasContext.fillStyle = drawColor;
   canvasContext.beginPath();
   var drawCircle = canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true)
-  // return drawCircle[Math.random()]
-
-  // centerX and centerY are the x & y position of the center of drawn circle.
   // 0 and Math.PI*2 are the angles in radians around the circle is being drawn.
+  // true to draw 
   canvasContext.fill()
 };
 
 function moveSnake(){
-  ballX += 5;
-  ballY += 5;
+  snakeX += 5;
+  snakeY += 5;
 
-  if (ballX > canvas.width){
+  if (snakeX >= canvas.width){
     // alert('Game is over!')
-    gameOver();
+    snakeReset();
+    //endGame();
+    
   } 
-  if (ballX < 0){
+  if (snakeX < 0){
     // alert('Game is over!')
-    gameOver();
+    snakeReset();
+    //endGame();
   }
-  if (ballY > canvas.height){
+  if (snakeY > canvas.height){
     // alert('Game is over!')
-    gameOver();
+    snakeReset();
+    //endGame();
   } 
-  if (ballX < 0){
+  if (snakeY < 0){
     // alert('Game is over!')
-    gameOver();
+    snakeReset();
+    //endGame();
   }
+
+  // endGame();
 }
 
-function gameOver(){
-  colorRect(100, 100, canvas.width/2, canvas.height/2, 'white');
-  // alert('Game is over!')
+function snakeReset (){
+  const ballx = snakeX;
 }
 
-function moveObject () {
-  canvas.addEventListener("keydown", function(e){
-    if(e.key === 40){
-      ballY += ballY;
+function changeSnakeDirection () {
+  document.onkeydown = function(e){
+    if(e.key === '40'){
+      snakeY += snakeY
       console.log('You clicked me!')
     }
-  })
+
+    // if(e.keyCode === 37){
+    //   snakeX = -snakeX
+    // }
+  };
 }
 
+// up 38
+// down 40
+// right 39
+// left 37
+
+
+
+// if the snake moves to the right/east
+  // option 1, move the snake to north
+  // optoin 2, move the snake to south
+
+//if the snake is moving to the left/west
+  // option 1, move the snake to north
+  // optoin 2, move the snake to south 
+
+// if the snake is moving to the up/north
+  // option 1, move the snake to the right
+  // option 2, move the snake to the left
+
+// if the snake is moving to the down/south
+  // option 1, move the snake to the right
+  // option 2, move the snake to the left
+
+
+
+// function endGame(){
+//   colorRect((canvas.width/2)-100, (canvas.height/2)-100, 200, 200, 'white');
+// }
 
 
 
 // move snake object with keyboard 
 // display apple randomly on window if/when snake eats an apple
 
-// function moveApple (canvas){
+// function appleResetPosition (canvas){
 //   if(ballX & ballY === ){
 //     for (position of canvas){
 //     }
 //   }
 //   displayApple = Math.random
+
+// return drawCircle[Math.random()]
+
 // }
