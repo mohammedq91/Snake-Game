cb3083a4d5a42f53c848b9a21625704468ea30a1
Mohammed <mohammedq91@gmail.com>
2021-07-05 12:32:16 -0400

    Figuring out moving the snake after arrow key is pressed

--- script.js
+++ script.js
@@ -9,13 +9,15 @@ window.onload = function (){
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
 
-  var framesPerSecond = 30;
-  setInterval(function(){
-    drawEverything();
-    moveSnake();
+  // var framesPerSecond = 30;
+  // setInterval(function(){
+  //   drawEverything();
+  //   moveSnake();
     
-  }, 1000/framesPerSecond);
-
+    
+  // }, 1000/framesPerSecond);
+  drawEverything()
+  gameOver();
   changeSnakeDirection();
 }
 
@@ -39,58 +41,75 @@ function colorCircle (centerX, centerY, radius, drawColor){
   canvasContext.fill()
 };
 
-function moveSnake(){
+function gameOver(){
   snakeX += 5;
   snakeY += 5;
 
   if (snakeX >= canvas.width){
     // alert('Game is over!')
     snakeReset();
-    //endGame();
+    canvasText((canvas.width/2)-78, (canvas.height/2)-60,'Game Over!', 'orange');
     
   } 
   if (snakeX < 0){
     // alert('Game is over!')
     snakeReset();
-    //endGame();
+    canvasText((canvas.width/2)-78, (canvas.height/2)-60,'Game Over!', 'orange');
   }
   if (snakeY > canvas.height){
     // alert('Game is over!')
     snakeReset();
-    //endGame();
+    canvasText((canvas.width/2)-78, (canvas.height/2)-60,'Game Over!', 'orange');
   } 
   if (snakeY < 0){
     // alert('Game is over!')
     snakeReset();
-    //endGame();
+    canvasText((canvas.width/2)-78, (canvas.height/2)-60,'Game Over!', 'orange');
   }
-
-  // endGame();
 }
 
-function snakeReset (){
-  const ballx = snakeX;
-}
+// function snakeReset (){
+//   const ballx = snakeX;
+// }
 
 function changeSnakeDirection () {
   document.onkeydown = function(e){
-    if(e.key === '40'){
-      snakeY += snakeY
-      console.log('You clicked me!')
+    
+    if(e.key === 'ArrowUp'){
+      console.log('Arrow up is clicked!')
+      arrowUp();
+      
     }
-
-    // if(e.keyCode === 37){
-    //   snakeX = -snakeX
-    // }
+    if(e.key === 'ArrowDown'){
+      console.log('Arrow down is clicked!')
+      moveSnake()
+    }
+    if(e.key === 'ArrowRight'){
+      console.log('Arrow right is clicked!')
+      colorRect(snakeX, canvas.height/2, 20, 20,'green');
+      moveSnake()
+    }
+    if(e.key === 'ArrowLeft'){
+      console.log('Arrow left is clicked!')
+      moveSnake()
+    }
   };
+}
+
+function arrowUp(){
+
+  if (snakeY === 0){
+    snakeX = 0;
+    snakeY += 5;
+  }
+  // colorRect(snakeY, canvas.height/2, 20, 20,'green');
+  colorRect(snakeX, snakeY, 20, 20,'green');
 }
 
 // up 38
 // down 40
 // right 39
 // left 37
-
-
 
 // if the snake moves to the right/east
   // option 1, move the snake to north
@@ -110,9 +129,18 @@ function changeSnakeDirection () {
 
 
 
-// function endGame(){
-//   colorRect((canvas.width/2)-100, (canvas.height/2)-100, 200, 200, 'white');
-// }
+function canvasText(startCoordinate, endCoordinate, text, textColor){
+  
+  const canvas = document.querySelector('#gameCanvas')
+  const canvasContext = canvas.getContext('2d')
+
+  colorRect((canvas.width/2)-100, (canvas.height/2)-100, 200, 200, 'white');
+
+  canvasContext.font = '30px orange'
+  canvasContext.fillStyle = textColor;
+  canvasContext.fillText(text, startCoordinate, endCoordinate)
+
+}
 
 
 
