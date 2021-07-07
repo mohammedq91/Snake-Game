9a7fd7d867c0d7e08e98dff4f6062c1380da2f65
Mohammed <mohammedq91@gmail.com>
2021-07-06 22:22:57 -0400

    Modifying drawSnake function and adding snake object

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var canvas;
 var canvasContext;
 var snakeX = 50;
-var snakeY = 20;
+// var snakeY = 20;
+
+let snake = {
+  body: [{x: 50, y: 50}],
+  direction: undefined,
+}
+console.log(snake.body[0].x);
 
 var appleX = 100;
 
@@ -9,37 +15,18 @@ window.onload = function (){
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
 
-  // var framesPerSecond = 30;
-  // setInterval(function(){
-  //   drawEverything();
-  //   moveSnake();
+  var framesPerSecond = 30;
+  setInterval(function(){
     
+  }, 1000/framesPerSecond);
+
+  drawGameCanvas();
+    drawSnake(snake.body[0].x, snake.body[0].y, 20, 20,'green');
+    drawApple(appleX,canvas.height/3, 10, 'red');
     
-  // }, 1000/framesPerSecond);
-  drawEverything()
-  gameOver();
-  changeSnakeDirection();
+    // gameOver();
+    changeSnakeDirection();
 }
-
-function drawEverything(){  
-  colorRect(0,0, canvas.width, canvas.height,'black');
-  colorRect(snakeX, canvas.height/2, 20, 20,'green');
-  colorCircle(appleX,canvas.height/3, 10, 'red');
-}
-
-function colorRect(leftX, topY, width, height, drawColor){
-  canvasContext.fillStyle = drawColor;
-  canvasContext.fillRect (leftX, topY, width, height);
-}
-
-function colorCircle (centerX, centerY, radius, drawColor){
-  canvasContext.fillStyle = drawColor;
-  canvasContext.beginPath();
-  var drawCircle = canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true)
-  // 0 and Math.PI*2 are the angles in radians around the circle is being drawn.
-  // true to draw 
-  canvasContext.fill()
-};
 
 function gameOver(){
   snakeX += 5;
@@ -65,19 +52,15 @@ function gameOver(){
     // alert('Game is over!')
     snakeReset();
     canvasText((canvas.width/2)-78, (canvas.height/2)-60,'Game Over!', 'orange');
-  }
-}
-
-// function snakeReset (){
-//   const ballx = snakeX;
-// }
+  };
+};
 
 function changeSnakeDirection () {
   document.onkeydown = function(e){
     
     if(e.key === 'ArrowUp'){
       console.log('Arrow up is clicked!')
-      arrowUp();
+      // arrowUp();
       
     }
     if(e.key === 'ArrowDown'){
@@ -85,23 +68,61 @@ function changeSnakeDirection () {
     }
     if(e.key === 'ArrowRight'){
       console.log('Arrow right is clicked!')
-      colorRect(snakeX, canvas.height/2, 20, 20,'green');
+      // colorRect(snakeX, canvas.height/2, 20, 20,'green');
     }
     if(e.key === 'ArrowLeft'){
       console.log('Arrow left is clicked!')
     }
   };
-}
+};
 
-function arrowUp(){
 
-  if (snakeY === 0){
-    snakeX = 0;
-    snakeY += 5;
-  }
-  // colorRect(snakeY, canvas.height/2, 20, 20,'green');
-  colorRect(snakeX, snakeY, 20, 20,'green');
-}
+// all drawing below
+function drawGameCanvas(){  
+  colorRect(0,0, canvas.width, canvas.height,'black');
+  //colorRect(snakeX, canvas.height/2, 20, 20,'green');
+};
+
+function drawSnake(leftX, topY, width, height, drawColor){
+  canvasContext.fillStyle = drawColor;
+  canvasContext.fillRect (leftX, topY, width, height);
+};
+
+function drawApple (centerX, centerY, radius, drawColor){
+  canvasContext.fillStyle = drawColor;
+  canvasContext.beginPath();
+  var drawCircle = canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true)
+  // 0 and Math.PI*2 are the angles in radians around the circle is being drawn.
+  // true to draw 
+  canvasContext.fill()
+};
+
+function canvasText(startCoordinate, endCoordinate, text, textColor){
+  const canvas = document.querySelector('#gameCanvas')
+  const canvasContext = canvas.getContext('2d')
+
+  colorRect((canvas.width/2)-100, (canvas.height/2)-100, 200, 200, 'white');
+
+  canvasContext.font = '30px orange'
+  canvasContext.fillStyle = textColor;
+  canvasContext.fillText(text, startCoordinate, endCoordinate)
+};
+
+function colorRect(leftX, topY, width, height, drawColor){
+  canvasContext.fillStyle = drawColor;
+  canvasContext.fillRect (leftX, topY, width, height);
+};
+
+// function arrowUp(){
+//   if (snakeY === 0){
+//     snakeX = 0;
+//     snakeY += 5;
+//   };
+//   colorRect(snakeY, canvas.height/2, 20, 20,'green');
+//   colorRect(snakeX, snakeY, 20, 20,'green');
+// };
+
+
 
 // if the snake moves to the right/east
   // option 1, move the snake to north
@@ -119,22 +140,9 @@ function arrowUp(){
   // option 1, move the snake to the right
   // option 2, move the snake to the left
 
-
-
-function canvasText(startCoordinate, endCoordinate, text, textColor){
-  
-  const canvas = document.querySelector('#gameCanvas')
-  const canvasContext = canvas.getContext('2d')
-
-  colorRect((canvas.width/2)-100, (canvas.height/2)-100, 200, 200, 'white');
-
-  canvasContext.font = '30px orange'
-  canvasContext.fillStyle = textColor;
-  canvasContext.fillText(text, startCoordinate, endCoordinate)
-
-}
-
-
+// function snakeReset (){
+//   const ballx = snakeX;
+// }
 
 // move snake object with keyboard 
 // display apple randomly on window if/when snake eats an apple
