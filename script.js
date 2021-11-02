1b7a7d5f7c28b13e28b0b544a3b70ea6c5dcd30a
Mohammed Qaralos <mohammedq91@gmail.com>
2021-11-02 17:22:20 -0400

    Grow Snake when it eats apple

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ var canvas;
 var canvasContext;
 
 let snake = {
-  body: [{x: 100, y: 100}],
+  headPosition: [{x: 100, y: 100}],
+  // size: [{width:20, height:20}],
   direction: undefined,
+}
+
+const apple = {
+  body: [{x: 400, y: 200}],
+  // color: red
 }
 //, {x: 50, y: 70}, {x: 50, y: 90}
 
@@ -11,23 +17,58 @@ window.onload = function (){
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
   
-
   var framesPerSecond = 30;
   setInterval(function(){
+    gameInitializer()
+  }, 10000/framesPerSecond);
+  
+}
+
+function gameInitializer(){
     drawGameWindow();
-
-    const appleRa
-
-
+    // const appleRa
     drawApple(canvas.width/2,canvas.height/2, 10, 'red');
     drawSnake();
     changeSnakeDirection();
     moveSnake();
-    // gameOver();
     
-  }, 10000/framesPerSecond);
-  wallDetection();
+
+    if (wallDetection()){
+      alert('game is alert');
+      // console.log('You clicked me')
+      gameOverText();
+    };
+  
 }
+
+// if snake head coordinate === apple coordinate, append snake body to snake object
+function growSnake(){
+  if (snake.headPosition[0].x === apple.body[0].x ){
+    snake.headPosition.push([snake.headPosition.x-snakeWidth, snake.headPosition.y]) = drawSnake();
+  }
+}
+
+function wallDetection(snakeWidth, snakeHeight){
+  if (snake.headPosition[0].x > canvas.width){
+    // snakeReset(); 
+  } 
+  if (snake.headPosition[0].x < 0){
+    // snakeReset(); 
+  }
+  if (snake.headPosition[0].y > canvas.height){
+    // snakeReset(); 
+  } 
+  if (snake.headPosition[0].y < 0){
+    // snakeReset();
+  };
+};
+
+// 2. snake body if head , same coordinate as any body part(s)
+// 3. Snake object, body array
+// 4. declone piece (i.e copy of snake body), then add to the body
+//    append to the snake body either on front or back end of the snake
+// 5. Grow Snake functionality.
+
 
 function changeSnakeDirection () {
   document.onkeydown = function(e){
@@ -57,63 +98,52 @@ function moveSnake(){
   };
 };
 
-function wallDetection(){
-  // snake.body.x += 5;
-  // snake.body.y += 5;
+// const textWidth = text.width
+  // wallDetection(textWidth)
 
-  if (snake.body[0].x > canvas.width){
-    // snakeReset();
-    gameOver((canvas.width/2)-78, (canvas.height/2)-60,'Game Over!', 'orange');
-  } 
-  if (snake.body[0].x < 0){
-    // snakeReset();
-    gameOver((canvas.width/2)-78, (canvas.height/2)-60,'Game Over!', 'orange');
-  }
-  if (snake.body[0].y > canvas.height){
-    // snakeReset();
-    gameOver((canvas.width/2)-78, (canvas.height/2)-60,'Game Over!', 'orange');
-  } 
-  if (snake.body[0].y < 0){
-    // snakeReset();
-    gameOver((canvas.width/2)-78, (canvas.height/2)-60,'Game Over!', 'orange');
-  };
-};
-
-function gameOver(startCoordinate, endCoordinate, text, textColor){
-  const canvas = document.querySelector('#gameCanvas')
-  const canvasContext = canvas.getContext('2d')
-
-  const rectWidth = 200;
-  const rectHeight = 200;
-
-  drawRect((canvas.width/2)-100, (canvas.height/2)-100, rectWidth, rectHeight, 'white');
-
-  canvasContext.font = '30px orange'
-  canvasContext.fillStyle = textColor;
-  canvasContext.fillText(text, startCoordinate, endCoordinate)
-};
-
-// all drawing below
-//---------------------------------
+//-----------------------------------------
+// All Drawing Below
 function drawGameWindow(){  
   drawRect(0,0, canvas.width, canvas.height,'black');
 };
+
+function drawApple (){
+  const appleWidth = 20;
+  const appleHeight = 20;
+  // snake.size.width;
+  // snake.size.height;
+  snake.body.forEach((part) => {
+    drawRect(part.x, part.y, snakeWidth, snakeHeight, 'green')
+  });
+
+
+}
+  
 
 function drawApple (centerX, centerY, radius, drawColor){
   canvasContext.fillStyle = drawColor;
   canvasContext.beginPath();
   canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true)
-  // 0 and Math.PI*2 are the angles in radians around the circle is being drawn.
-  // true to draw circle clock or clock-wise circle?
+  // "0" and "Math.PI*2" are the angles in radians around the circle is being drawn.
+  // "true" to draw circle clock or clock-wise circle?
   canvasContext.fill()
 };
 
 function drawSnake(){
   const snakeWidth = 20;
   const snakeHeight = 20;
+  // snake.size.width;
+  // snake.size.height;
   snake.body.forEach((part) => {
     drawRect(part.x, part.y, snakeWidth, snakeHeight, 'green')
   });
+  // wallDetection(snakeWidth,snakeHeight)
+};
+
+function gameOverText(text, startCoordinate, endCoordinate){
+  canvasContext.font = '100px orange';
+  canvasContext.fillStyle = 'orange';
+  canvasContext.fillText(text, startCoordinate, endCoordinate)
 };
 
 function drawRect(leftX, topY, width, height, drawColor){
