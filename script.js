ee02b018ac313f53e7016457b489c2a46ad07325
Mohammed Qaralos <mohammedq91@gmail.com>
2021-11-13 18:41:09 -0500

    Is_Snake_Touching_Itself?

--- script.js
+++ script.js
@@ -10,62 +10,58 @@ let snake = {
   // speed: [{x: 0, y: 0}],
   // body: [{x: snake.head.x , y: snake.head.y}],
   direction: undefined
-}
+};
 
 let apple = {
-  x:  Math.floor(Math.random() * (canvas.width - gridSize)/20) * 20 , 
-  y:  Math.floor(Math.random() * (canvas.height - gridSize)/20) * 20
-}
+  x: Math.floor(Math.random() * (canvas.width - gridSize)/20) * 20 , 
+  y: Math.floor(Math.random() * (canvas.height - gridSize)/20) * 20
+};
 
-const gameSettings = {
-  isGameRunning: 0,
-  snakeSize: 20
-}
+// const gameSettings = {
+//   isGameRunning: 0,
+//   snakeSize: 20
+// };
 
 window.onload = function (){  
   var framesPerSecond = 20;
   playGame = setInterval(function(){
     gameInitializer()
   }, 4000/framesPerSecond);
-}
-
-console.log("Canvas.width index is:", canvas.width)
-console.log("Canvas.height index is:", canvas.height)
-console.log("canvas 0 width index is:", )
+};
 
 function gameInitializer(){
-    drawGameWindow();
-    drawApple();
-    changeSnakeDirection();
-    moveSnake();
-    drawSnake();
-    isAppleEaten();
-    // is_Snake_Hitting_Wall();
-    if (is_Snake_Hitting_Wall()) gameOver();
-}
+  drawGameWindow();
+  drawApple();
+  changeSnakeDirection();
+  moveSnake();
+   
+  if(isAppleEaten) {
+    growSnake()
+    drawNewSnakePart();
+    placeApple()
+  };
+
+  if(is_Snake_Hitting_Wall() || is_Snake_Touching_Itself()) gameOver();
+};
 
 function placeApple(){
     apple = { x: Math.floor(Math.random() * (canvas.width - gridSize)/20) * 20 , 
               y: Math.floor(Math.random() * (canvas.height - gridSize)/20) * 20}
-}
+};
 
 function isAppleEaten(){
   let head = snake.head[0];
-  if(head.x === apple.x && head.y === apple.y){
-    growSnake();
-    placeApple();
-  };
-}
+  return (head.x === apple.x && head.y === apple.y)
+};
 
 function growSnake(){
   const body = snake.head[0] // const body = {x: snake.head.x , y: snake.head.y} 
   snake.head.push(body)
-}
+};
 
 function changeSnakeDirection () {
   document.onkeydown = function(e){
     var keyboard = e.key;
-    // if(keyboard === ' ') gameInitializer();
     if(keyboard === 'ArrowUp' && snake.direction != "down") snake.direction = "up";
     if(keyboard === 'ArrowDown' && snake.direction != "up") snake.direction = "down"; 
     if(keyboard === 'ArrowRight' && snake.direction != "left") snake.direction = "right";
@@ -98,15 +94,54 @@ function is_Snake_Hitting_Wall(){
     snake.head[0].x === canvas.width ||
     snake.head[0].y < 0 ||
     snake.head[0].y === canvas.height)
-  };
-  
+};
+
+// function is_Snake_Touching_Itself(){
+//   for (let i = 1; i < snake.head.length; i++ ){
+//     if(snake.head[0].x === snake.head[i].x && snake.head[0].y === snake.head[i].y) 
+//       return true
+//   }
+//   return false
+// };
+
+function is_Snake_Touching_Itself(){
+  for (let i = 1; i < snake.head.length; i++ ){
+    if(i <= 3){
+      continue
+    };
+    if (i > 3){
+      return (snake.head[0].x === snake.head[i].x && snake.head[0].y === snake.head[i].y) 
+    };
+  return false
+};
+
+// 2 parts   i = 1
+// 3 parts   i = 2
+// 4 parts   i = 3
+// 5 parts   i =
+
+//TAKES IN INPUT 
+//DOES SOMETHING TO INPUT 
+//RETURNS THE MODIFIED INPUT 
+
+// Goal
+// if snake head's coordinates (x & y) equal to any of snake body's coordinates (x & y)
+// game over
+
+// 1. Loop through each of the snake's part 
+// 2. Create conditional statement for Snake's body coordinates (x & y)
+// 3. If snake's head coordinates (x, y) equals to any of the snake's parts coordiantes (x,y)
+// 4. Retrun true.
+
+//   0         1       2    N - 1
+//[{HEAD}, {PART1}, {PART2}, ...]
 function gameOver(){
-    endGame = clearInterval(playGame);
-    console.log("Game is Over!")
-  }
+  endGame = clearInterval(playGame);
+  console.log("Game is Over!")
+};
   
-  // const textWidth = textHeight = 300;
-  // drawText('30px Arial', 'orange','Game Over!', textWidth, textHeight)
+// const textWidth = textHeight = 300;
+// drawText('30px Arial', 'orange','Game Over!', textWidth, textHeight)
 //-----------------All Drawing Below ------------------------------------
 function drawGameWindow(){  
   drawRect(0,0, canvas.width, canvas.height,'saddlebrown');
@@ -116,7 +151,7 @@ function drawApple(){
     drawRect(apple.x, apple.y , gridSize, gridSize, 'green') 
 }
 
-function drawSnake(){
+function drawNewSnakePart(){
   snake.head.forEach((part) => {
     drawRect(part.x, part.y, gridSize, gridSize, 'black')
   });
@@ -146,4 +181,4 @@ function drawRect(posX, posY, width, height, color){
 //   // "0" and "Math.PI*2" are the angles in radians around the circle is being drawn.
 //   // "true" to draw circle clock or clock-wise circle?
 //   canvasContext.fill()
-// };
+//  }; 
